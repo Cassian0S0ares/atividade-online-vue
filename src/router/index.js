bf125ab52ed56bdbe5a4456c1adf1e840d90f5a2
Cassian0s0ares <masterticole@gmail.com>
2026-09-22 22:10:23 -0300

    haha

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,13 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import TarefasView from '../views/TarefasView.vue'
- 
+
 const routes = [
   { path: '/', component: HomeView },
   { path: '/tarefas', component: TarefasView }
 ]
- 
+
 export default createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes
 })
- 
